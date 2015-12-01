41a1b60d16b69cdf1629a5fdcff7fd28cf80f1cf
Henry Firth <henrynf12@gmail.com>
2015-12-01 13:16:11 -0600

    Add missing ESPN stats

--- app/models/batter.rb
+++ app/models/batter.rb
@@ -1,2 +1,27 @@
 class Batter < ActiveRecord::Base
+
+  def tb
+    singles + doubles*2 + triples * 3 + hr * 4
+  end
+
+  def ppa
+    pitches.to_f / pa.to_f
+  end
+
+  def rc
+    r + rbi
+  end
+
+  def sbn
+    sb - cs
+  end
+
+  def sac
+    sh + sf
+  end
+
+  def xbh
+    doubles + triples + hr
+  end
+
 end

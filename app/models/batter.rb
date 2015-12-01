184218b6437c7649c0f4b981d11b7645985eae61
Henry Firth <henrynf12@gmail.com>
2015-12-01 15:29:17 -0600

    Add normalization method for stats

--- app/models/batter.rb
+++ app/models/batter.rb
@@ -1,5 +1,7 @@
 class Batter < ActiveRecord::Base
 
+  BAD_STATS = [:so, :cs, :gdp]
+
   def tb
     singles + doubles*2 + triples * 3 + hr * 4
   end
@@ -22,6 +24,13 @@ class Batter < ActiveRecord::Base
 
   def xbh
     doubles + triples + hr
+  end
+
+  def normalized(stat_method)
+    stats = Batter.all.map { |batter| batter.method(stat_method).call }
+    result = ((method(stat_method).call - stats.min).to_f / (stats.max - stats.min).to_f)
+
+    (BAD_STATS.include? stat_method) ? 1 - result : result
   end
 
 end

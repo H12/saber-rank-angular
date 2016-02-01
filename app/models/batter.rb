f8f2265706ff6ae7f00a480b793a0041616f62c8
Henry Firth <henrynf12@gmail.com>
2016-02-01 15:02:12 -0600

    Move min/max query to controller

--- app/models/batter.rb
+++ app/models/batter.rb
@@ -4,15 +4,19 @@ class Batter < ActiveRecord::Base
 
   def as_json(options = {})
     response = {'name' => name, 'team' => team}
-    options[:stats].each { |stat| response[stat] = method(stat).call }
+
+    options[:stats].each { |stat, min_max_values| response[stat] = method(stat).call }
     response.each { |key, value| response[key] = sprintf('%.3f', value) if value.is_a?(Float) }
+
     stat_value = 0
-    options[:stats].each do |stat|
-      stat_value += normalize(stat, stat_min(stat), stat_max(stat)).to_f
+    options[:stats].each do |stat, min_max_values|
+      stat_value += normalize(stat, min_max_values['min'], min_max_values['max']).to_f
     end
     response['stat'] = ((stat_value * 60 / options[:stats].length) + 20).to_i
+
     mappings = {'singles' => 's', 'doubles' => 'd', 'triples' => 't', 'pitches' => 'p'}
     response = response.map {|k,v| mappings.keys.include?(k) ? [mappings[k], v] : [k,v]}.to_h
+
     response
   end
 
@@ -40,52 +44,50 @@ class Batter < ActiveRecord::Base
     doubles + triples + hr
   end
 
-  def stat_min(stat_method)
-    case stat_method
-    when "tb"
-      stat_query("tb", "singles + doubles * 2 + triples * 3 + hr * 4", "min")
-    when "ppa"
-      stat_query("ppa", "pitches / pa", "min")
-    when "rc"
-      stat_query("rc", "r + rbi - hr", "min")
-    when "sbn"
-      stat_query("sbn", "sb - cs", "min")
-    when "sac"
-      stat_query("sac", "sh + sf", "min")
-    when "xbh"
-      stat_query("xbh", "doubles + triples + hr", "min")
-    else
-      Batter.minimum(stat_method)
-    end
-  end
-
-  def stat_max(stat_method)
-    case stat_method
-    when "tb"
-      stat_query("tb", "singles + doubles * 2 + triples * 3 + hr * 4", "max")
-    when "ppa"
-      stat_query("ppa", "pitches / pa", "max")
-    when "rc"
-      stat_query("rc", "r + rbi - hr", "max")
-    when "sbn"
-      stat_query("sbn", "sb - cs", "max")
-    when "sac"
-      stat_query("sac", "sh + sf", "max")
-    when "xbh"
-      stat_query("xbh", "doubles + triples + hr", "max")
-    else
-      Batter.maximum(stat_method)
-    end
-  end
-
   def normalize(stat_method, min, max)
     result = ((method(stat_method).call.to_f - min) / (max - min))
     (BAD_STATS.include? stat_method) ? 1 - result : result
   end
 
-  private
+  def self.stat_min(stat_method)
+    case stat_method
+    when "tb"
+      Batter.stat_query("tb", "singles + doubles * 2 + triples * 3 + hr * 4", "MIN")
+    when "ppa"
+      Batter.stat_query("ppa", "pitches / pa", "MIN")
+    when "rc"
+      Batter.stat_query("rc", "r + rbi - hr", "MIN")
+    when "sbn"
+      Batter.stat_query("sbn", "sb - cs", "MIN")
+    when "sac"
+      Batter.stat_query("sac", "sh + sf", "MIN")
+    when "xbh"
+      Batter.stat_query("xbh", "doubles + triples + hr", "MIN")
+    else
+      Batter.minimum(stat_method)
+    end
+  end
 
-  def stat_query(column_name, calculation, option)
+  def self.stat_max(stat_method)
+    case stat_method
+    when "tb"
+      Batter.stat_query("tb", "singles + doubles * 2 + triples * 3 + hr * 4", "MAX")
+    when "ppa"
+      Batter.stat_query("ppa", "pitches / pa", "MAX")
+    when "rc"
+      Batter.stat_query("rc", "r + rbi - hr", "MAX")
+    when "sbn"
+      Batter.stat_query("sbn", "sb - cs", "MAX")
+    when "sac"
+      Batter.stat_query("sac", "sh + sf", "MAX")
+    when "xbh"
+      Batter.stat_query("xbh", "doubles + triples + hr", "MAX")
+    else
+      Batter.maximum(stat_method)
+    end
+  end
+
+  def self.stat_query(column_name, calculation, option)
     ActiveRecord::Base.connection.execute("SELECT #{option}(#{calculation}) AS #{column_name} FROM batters")[0][column_name].to_f
   end
 

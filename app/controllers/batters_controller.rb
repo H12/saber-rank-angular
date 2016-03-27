792783c8cb885bb48e155e7deb1dafba2fdf64c2
H12 <henrynf12@gmail.com>
2016-03-27 11:28:11 -0500

    Properly check for params[:stats]

--- app/controllers/batters_controller.rb
+++ app/controllers/batters_controller.rb
@@ -3,7 +3,7 @@ class BattersController < ApplicationController
     params[:min_pa] ||= 500
     @batters = Batter.where("pa >= :min_pa", {min_pa: params[:min_pa]})
 
-    params[:stats] ? stats = JSON.parse(params[:stats]) : stats = ['avg', 'hr', 'r', 'rbi', 'sb']
+    params[:stats] && JSON.parse(params[:stats]).length > 0 ? stats = JSON.parse(params[:stats]) : stats = ['avg', 'hr', 'r', 'rbi', 'sb']
     stats = stats.map { |stat| [stat, {'min' => Batter.stat_min(stat, params[:min_pa]), 'max' => Batter.stat_max(stat, params[:min_pa])}] }.to_h
 
     respond_to do |format|
